7bba0d53153f2a90c01fe4342f23e96253782749
Knut Kirkhorn <knutkirk@hotmail.com>
2020-03-09 17:20:02 +0100

    Add ESLint

--- cli.js
+++ cli.js
@@ -6,34 +6,34 @@ const fs = require('fs');
 const chalk = require('chalk');
 const Ora = require('ora');
 const logSymbols = require('log-symbols');
+const path = require('path');
 const spinner = new Ora();
 
 const defaultConfiguration = {
     default: {
-        name: "United States Dollar",
-        code: "USD",
-        symbol: "$"
+        name: 'United States Dollar',
+        code: 'USD',
+        symbol: '$'
     },
     quantity: 1,
     autorefresh: 15,
     apiKey: ''
 };
-const configFile = __dirname + '/config.json';
+const configFile = path.join(__dirname, './config.json');
 let config;
 
 try {
-    config = require(configFile);
+    // eslint-disable-next-line global-require
+    config = require('./config.json');
 } catch (e) {
     // Set the config to the default if its not found in the file
     config = defaultConfiguration;
 }
 
 let defaultCurrency = config.default;
-let quantity = config.quantity;
-let autorefresh = config.autorefresh;
-let apiKey = config.apiKey;
+let {quantity, autorefresh, apiKey} = config;
+// eslint-disable-next-line no-unused-vars
 let autorefreshTimer;
-
 
 const cli = meow(`
         Usage
@@ -105,7 +105,7 @@ const cli = meow(`
     }
 });
 
-// Search currency list if input currency code matches any 
+// Search if input currency code matches any in the currency list
 function isValidCurrencyCode(currencyCode) {
     currencyCode = currencyCode.toUpperCase();
     let currency;
@@ -152,14 +152,14 @@ function exitError(error) {
 function saveConfig(newConfig) {
     return new Promise((resolve, reject) => {
         // Save new config file
-        fs.writeFile(configFile, newConfig, function(error) {
+        fs.writeFile(configFile, newConfig, error => {
             if (error) {
                 reject();
                 return;
-            } else {
-                resolve();
-                return;
             }
+
+            resolve();
+            return;
         });
     });
 }
@@ -170,17 +170,16 @@ async function checkAllFlags() {
     if (cli.flags.s !== undefined) {
         defaultCurrency = isValidCurrencyCode(cli.flags.s);
 
-        const newConfig = JSON.stringify(
-            {
-                default: {
-                    name: defaultCurrency.name,
-                    code: defaultCurrency.code,
-                    symbol: defaultCurrency.symbol
-                },
-                quantity: quantity,
-                autorefresh: autorefresh,
-                apiKey: apiKey
-            }, null, 4);
+        const newConfig = JSON.stringify({
+            default: {
+                name: defaultCurrency.name,
+                code: defaultCurrency.code,
+                symbol: defaultCurrency.symbol
+            },
+            quantity,
+            autorefresh,
+            apiKey
+        }, null, 4);
 
         try {
             await saveConfig(newConfig);
@@ -198,17 +197,16 @@ async function checkAllFlags() {
             if (quantity !== cli.flags.q) {
                 // Save the new value of `quantity`
                 quantity = cli.flags.q;
-                const newConfig = JSON.stringify(
-                    {
-                        default: {
-                            name: defaultCurrency.name,
-                            code: defaultCurrency.code,
-                            symbol: defaultCurrency.symbol
-                        },
-                        quantity: quantity,
-                        autorefresh: autorefresh,
-                        apiKey: apiKey
-                    }, null, 4);
+                const newConfig = JSON.stringify({
+                    default: {
+                        name: defaultCurrency.name,
+                        code: defaultCurrency.code,
+                        symbol: defaultCurrency.symbol
+                    },
+                    quantity,
+                    autorefresh,
+                    apiKey
+                }, null, 4);
 
                 try {
                     await saveConfig(newConfig);
@@ -232,7 +230,7 @@ async function checkAllFlags() {
         if (cli.flags.p === 'h') {
             try {
                 const percentage = await btcValue.getPercentageChangeLastHour();
-                printPercentage(percentage + '%');
+                printPercentage(`${percentage}%`);
             } catch (e) {
                 console.log(e);
                 exitError('Please check your internet connection');
@@ -240,7 +238,7 @@ async function checkAllFlags() {
         } else if (cli.flags.p === 'd' || cli.flags.p === '') {
             try {
                 const percentage = await btcValue.getPercentageChangeLastDay();
-                printPercentage(percentage + '%');
+                printPercentage(`${percentage}%`);
             } catch (e) {
                 console.log(e);
                 exitError('Please check your internet connection');
@@ -248,7 +246,7 @@ async function checkAllFlags() {
         } else if (cli.flags.p === 'w') {
             try {
                 const percentage = await btcValue.getPercentageChangeLastWeek();
-                printPercentage(percentage + '%');
+                printPercentage(`${percentage}%`);
             } catch (e) {
                 console.log(e);
                 exitError('Please check your internet connection');
@@ -256,29 +254,27 @@ async function checkAllFlags() {
         } else {
             exitError('Invalid percentage input. Check `btc-value --help`.');
         }
-    } else {
+    } else if (cli.flags.c) {
         // If `d` flag is set => return value as decimal
         // USD is the default currency in the API
         // If `c` flag is set => convert to other currency
         // Print value of given `quantity` or just 1 BTC
-        if (cli.flags.c) {
-            const currency = isValidCurrencyCode(cli.flags.c);
-        
-            try {
-                const value = await btcValue({currencyCode: cli.flags.c, isDecimal: cli.flags.d, quantity: multiplier});
-                printOutput(`${chalk.yellow(currency.symbol)}${value}`);
-            } catch (e) {
-                console.log(e);
-                exitError('Please check your internet connection');
-            }
-        } else {
-            try {
-                const value = await btcValue({currencyCode: defaultCurrency.code, isDecimal: cli.flags.d, quantity: multiplier});
-                printOutput(`${chalk.yellow(defaultCurrency.symbol)}${value}`);
-            } catch (e) {
-                console.log(e);
-                exitError('Please check your internet connection');
-            }
+        const currency = isValidCurrencyCode(cli.flags.c);
+
+        try {
+            const value = await btcValue({currencyCode: cli.flags.c, isDecimal: cli.flags.d, quantity: multiplier});
+            printOutput(`${chalk.yellow(currency.symbol)}${value}`);
+        } catch (e) {
+            console.log(e);
+            exitError('Please check your internet connection');
+        }
+    } else {
+        try {
+            const value = await btcValue({currencyCode: defaultCurrency.code, isDecimal: cli.flags.d, quantity: multiplier});
+            printOutput(`${chalk.yellow(defaultCurrency.symbol)}${value}`);
+        } catch (e) {
+            console.log(e);
+            exitError('Please check your internet connection');
         }
     }
 
@@ -302,7 +298,7 @@ if (cli.flags.l) {
         if (i % 9 === 0) {
             currencyOutprint += '\n      ';
         }
-        
+
         currencyOutprint += btcValue.currencies[i].code;
         if (i !== btcValue.currencies.length - 1) {
             currencyOutprint += ', ';
@@ -317,7 +313,7 @@ if (cli.flags.l) {
     // If `r` falg is set => reset configuration file
     if (cli.flags.r) {
         const newConfig = JSON.stringify(defaultConfiguration, null, 4);
-    
+
         try {
             await saveConfig(newConfig);
             console.log(chalk.green(`${logSymbols.success} Default configuration reset to: ${defaultConfiguration.default.name} (${defaultConfiguration.default.symbol})`));
@@ -331,19 +327,18 @@ if (cli.flags.l) {
     // If `k` flag is set => set the API key
     if (cli.flags.k) {
         apiKey = cli.flags.k;
-        
-        const newConfig = JSON.stringify(
-            {
-                default: {
-                    name: defaultCurrency.name,
-                    code: defaultCurrency.code,
-                    symbol: defaultCurrency.symbol
-                },
-                quantity: quantity,
-                autorefresh: autorefresh,
-                apiKey: apiKey
-            }, null, 4);
-    
+
+        const newConfig = JSON.stringify({
+            default: {
+                name: defaultCurrency.name,
+                code: defaultCurrency.code,
+                symbol: defaultCurrency.symbol
+            },
+            quantity,
+            autorefresh,
+            apiKey
+        }, null, 4);
+
         try {
             await saveConfig(newConfig);
             console.log(chalk.green(`${logSymbols.success} API key is set`));
@@ -360,6 +355,6 @@ if (cli.flags.l) {
     } else {
         btcValue.setApiKey(apiKey);
     }
-    
+
     checkAllFlags();
 })();
